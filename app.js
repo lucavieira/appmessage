920bb732c791e27dec7a5971901f76f8eac451c6
lucavieira <lukasveras14@gmail.com>
2022-09-05 19:42:36 -0300

    ajustes

--- app.js
+++ app.js
@@ -12,8 +12,6 @@ const app = express()
 
 const messages = require('./routes/user') // Rotas
 
-app.use('/favicon.ico', express.static(__dirname + 'public/images/favicon.ico'));
-
 // Session
 app.use(session({
   secret: 'message_app',
